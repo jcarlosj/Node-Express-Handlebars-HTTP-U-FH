432359cc21f9b7d47998c187ce8f772d2d2be7cd
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-11-02 14:53:22 -0500

    [+] Express: Se escriben datos JSON en el cuerpo del documento

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const express = require( 'express' ),     /** Framework para Node */
 
 /** Rutas */
 app .get( '/', ( request, response ) => {
-    response .send( '<h1>WebServer usando Express</h1>' );
+    let data = {
+        nombre: 'Juliana',
+        edad: 29,
+        url: response .url
+    }
+
+    response .send( data );     /** send: detecta que los datos están en formato JSON y lo formatea adecuadamente */
+});
+app .get( '/data', ( request, response ) => {
+    response .send( '<h2>Data</h2>' );
 });
 
 /** Crea servidor con Node */
-app .listen( 3000 );
-
-console .log( 'Escuchando en el puerto 3000' );
+app .listen( 3000, () => {
+    console .log( 'Escuchando en el puerto 3000' );
+});
