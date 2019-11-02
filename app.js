5041483e4d0cddbde2d6c7618a244f61adf50879
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-11-02 14:31:35 -0500

    [+] http: Se escriben datos JSON en el cuerpo del documento

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const http = require( 'http' );     /** Paquete nativo de Node */
 
 /** Crea servidor con Node */
 http .createServer( ( request, response ) => {
-    response .write( '<h1>Web Server con Node</h1>' );
-    response .end();    
+
+    let data = {
+        nombre: 'Juliana',
+        edad: 29,
+        url: request .url
+    }
+
+    response .writeHead( 200, { 'Content-Type': 'application/json' });  /** Agrega a la cabecera el tipo de contenido que se va a usar */
+    response .write( JSON .stringify( data ) );                         /** Se escribe el contenido en el cuerpo de la página como una cadena de texto */
+    response .end();                                                    /** Se finaliza la respuesta del servidor */
 }) .listen( 8080 );
 
 console .log( 'Escuchando en el puerto 8080' );
