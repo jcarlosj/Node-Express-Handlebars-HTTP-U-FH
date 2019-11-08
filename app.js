fc68e106bfd86e2ed5c99d28ea974c9f124d8936
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-11-08 10:56:30 -0500

    [+] Handlebars > Helpers: Año Actual y Capitalizar Texto

--- app.js
+++ app.js
@@ -8,17 +8,28 @@ app .use( express .static( __dirname + '/public' ) );       /** Define directori
 hbs .registerPartials( __dirname + '/views/partials' );     /** Define directorio para partials */
 app .set( 'view engine', 'hbs' );                           /** Define el motor de plantillas Handlebars para Express */
 
+/** Helpers */
+hbs .registerHelper( 'getCurrentYear', () => {              /** Define un Helper para HandleBars */
+    return new Date() .getFullYear();
+});
+hbs .registerHelper( 'capitalize', ( text ) => {
+    let words = text .split( ' ' );
+
+    words .forEach( ( word, idx ) => {
+        words[ idx ] = word .charAt( 0 ) .toUpperCase() + word  .slice( 1 ) .toLowerCase();     /** Colola mayúscula solo a la primera letra de la palabra */
+    });
+
+    return words .join( ' ' );
+});
+
 /** Rutas */
 app .get( '/', ( request, response ) => {
     response .render( 'home', {
-        title: 'Web Server',
-        anio: new Date() .getFullYear()
+        title: 'Web Server'
     } );     /** render: el archivo con el nombrepasado y lo renderiza en la página */
 });
 app .get( '/contactenos', ( request, response ) => {
-    response .render( 'contactenos', {
-        anio: new Date() .getFullYear()
-    } );
+    response .render( 'contactenos' );
 });
 
 /** Crea servidor con Node */
